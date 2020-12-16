5a4487e00aa156a74abb7e969507fc8f16670e17
PublicCriminali <01123869@pw.edu.pl>
2020-12-16 21:54:39 +0000

    Rozwiazanie 4

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -11,11 +11,11 @@ using namespace std;
 vector< char > foo(list< Human >& people)
 {
     vector< char > ret_v(people.size());
-    auto monster=[](Human& H;){
-      if(H.isMonster()) return 'y';
-      else return 'n';
-    }
+
     for_each(people.begin(),people.end(),[](Human& H;){return H.birthday();});
-    transform(people.begin(),people.end(),ret_v.begin(),monster);
+    transform(people.begin(),people.end(),ret_v.begin(),[](Human& H;){
+      if(H.isMonster()) return 'n';
+      else return 'y';
+    });
     return ret_v;
 }
