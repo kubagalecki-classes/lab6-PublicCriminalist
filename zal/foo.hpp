f1df91f168b9d1261abbf7cd379845344fe29f7f
PublicCriminali <01123869@pw.edu.pl>
2020-12-16 22:01:34 +0000

    Rozwiazanie final 2

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -14,7 +14,7 @@ vector< char > foo(list< Human >& people)
     auto monster=[](Human& H){
       if(H.isMonster()) return 'n';
       else return 'y';
-    }
+    };
     for_each(people.begin(),people.end(),[](Human& H){return H.birthday();});
     transform(people.begin(),people.end(),ret_v.rbegin(),monster);
     return ret_v;
